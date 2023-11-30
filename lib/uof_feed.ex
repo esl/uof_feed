906bd7cca3d62832243c63bbf170a34f949ddd31
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2023-11-30 17:15:28 +0100

    Save Mix project scaffolding

---
+++ lib/uof_feed.ex
@@ -0,0 +1,18 @@
+defmodule UofFeed do
+  @moduledoc """
+  Documentation for `UofFeed`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UofFeed.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/uof_feed_test.exs
@@ -0,0 +1,8 @@
+defmodule UofFeedTest do
+  use ExUnit.Case
+  doctest UofFeed
+
+  test "greets the world" do
+    assert UofFeed.hello() == :world
+  end
+end
